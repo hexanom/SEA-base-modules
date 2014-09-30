e40bfed6b61d926fed1f5fad5b822226e8010426
Robin Ricard <ricard.robin@gmail.com>
2014-09-30 10:30:17 +0200

    Base sched

--- kernel.c
+++ kernel.c
@@ -1,21 +1,26 @@
-int divide(int dividend, int divisor) {
-  int result = 0;
-  int remainder = dividend;
-  while (remainder >= divisor) {
-    result++;
-    remainder -= divisor;
+#include "sched.h"
+struct ctx_s ctx_ping;
+struct ctx_s ctx_pong;
+struct ctx_s ctx_init;
+
+void ping() {
+  while ( 1 ) {
+    switch_to(&ctx_pong);
   }
-  return result;
 }
 
-int compute_volume(int rad) {
-  int rad3 = rad * rad * rad;
-  return divide(4*355*rad3, 3*113);
+void pong() {
+  while ( 1 ) {
+    switch_to(&ctx_ping);
+  }
 }
 
 int kmain( void ) {
-  int radius = 5;
-  int volume;
-  volume = compute_volume(radius);
-  return volume;
+  init_hw();
+  init_ctx(&ctx_ping, ping, STACK_SIZE);
+  init_ctx(&ctx_pong, pong, STACK_SIZE);
+  current_ctx = &ctx_init;
+  switch_to(&ctx_ping);
+  /* Pas atteignable vues nos 2 fonctions */
+  return(0);
 }
---
+++ sched.h
@@ -0,0 +1,3 @@
+struct ctx_s {
+
+};
